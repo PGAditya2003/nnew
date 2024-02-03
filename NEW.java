cce17adcd32162a5aa2d1388ac023a4290f30487
PGAditya2003 <adimaharshi20@gmail.com>
2024-02-03 11:33:36 +0530

    java file updated

---
+++ NEW.java
@@ -0,0 +1,20 @@
+/**
+ * NEW
+ */
+public class NEW {
+
+    void GIT(){
+        System.out.println("this is git");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Name`s Bond");
+        NEW ob = NEW();
+        for(int i =0 ;i<5 ;i++){
+            System.out.println();
+            ob.GIT();
+        }
+
+    }
+    
+}
