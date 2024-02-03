ae5f07cc7f8a4076eb1c6cb386b89508f0e98828
NME <adimaharshi20@gmail.com>
2024-02-03 11:59:02 +0530

    vscode.

--- NEW.java
+++ NEW.java
@@ -9,7 +9,7 @@ public class NEW {
 
     public static void main(String[] args) {
         System.out.println("Name`s Bond");
-        NEW ob = NEW();
+        NEW ob = new NEW();
         for(int i =0 ;i<5 ;i++){
             System.out.println();
             ob.GIT();
